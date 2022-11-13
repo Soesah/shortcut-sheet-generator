4a7e2f8ac83cecb839b74271022fdb0ea0fb2585
Carl Giesberts <carl@front-crafter.nl>
2022-11-13 21:52:40 +0100

    Load sheets and show on list

--- src/components/SheetList.vue
+++ src/components/SheetList.vue
@@ -1,6 +1,25 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { computed, onMounted } from 'vue';
+import { useStore } from 'vuex';
+import { Actions } from '@/store';
+
+const store = useStore();
+
+const sheets = computed(() => store.state.sheets);
+
+onMounted(() => {
+  store.dispatch(Actions.GetSheets);
+});
+</script>
 <template>
   <div>
     <h2>Sheets</h2>
+    <ul>
+      <li v-for="sheet in sheets" :key="sheet.id">
+        <RouterLink :to="`/sheets/${sheet.id}`">{{
+          sheet.description
+        }}</RouterLink>
+      </li>
+    </ul>
   </div>
 </template>
--- src/store.ts
+++ src/store.ts
@@ -3,20 +3,40 @@ import { drawerStore } from './stores/drawer.store';
 import { modalStore } from './stores/modal.store';
 // models
 // services
+import sheetService from './services/sheet.service';
+import { Sheet } from './models/sheet.model';
 
-export interface SSGState {}
+export interface SSGState {
+  sheets: Sheet[];
+}
 
-export enum Mutations {}
+export enum Mutations {
+  SetSheets = 'setSheets',
+}
 
-export enum Actions {}
+export enum Actions {
+  GetSheets = 'getSheets',
+}
 
 export const store = createStore<SSGState>({
   modules: {
     ds: drawerStore,
     ms: modalStore,
   },
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    sheets: [],
+  },
+  mutations: {
+    [Mutations.SetSheets]: (state: SSGState, sheets: Sheet[]) => {
+      state.sheets = sheets;
+    },
+  },
+  actions: {
+    [Actions.GetSheets]: async ({ commit }) => {
+      const res = await sheetService.getList();
+
+      commit(Mutations.SetSheets, res.data);
+    },
+  },
   getters: {},
 });
